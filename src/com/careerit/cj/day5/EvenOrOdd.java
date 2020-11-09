c36c08b31fcac6837156a9d5fd1d769954fb4560
Lakshman <lakshman.avula@outlook.com>
2020-11-09 07:34:26 +0530

    Added input statement

--- src/com/careerit/cj/day5/EvenOrOdd.java
+++ src/com/careerit/cj/day5/EvenOrOdd.java
@@ -1,9 +1,11 @@
 package com.careerit.cj.day5;
 
+import java.util.Scanner;
+
 public class EvenOrOdd {
 
           public static void main(String... args){
             
-            
+        	  		Scanner sc = new Scanner(System.in);
           }
 }
