c2bbc321bce892ec5c92c3bb969e09224e17b730
Lakshman <lakshman.avula@outlook.com>
2020-11-09 08:09:51 +0530

    Changed input message

--- src/com/careerit/cj/day5/EvenOrOdd.java
+++ src/com/careerit/cj/day5/EvenOrOdd.java
@@ -7,7 +7,7 @@ public class EvenOrOdd {
           public static void main(String... args){
         	  	
         	  		Scanner sc = new Scanner(System.in);
-        	  		System.out.println("Enter the num:");
+        	  		System.out.println("Enter integer value to check even or odd");
         	  		int num = sc.nextInt();
         	  		// To check even or odd
         	  		if(num % 2 == 0) {   
