374be75cd4190a63df10449b8745124cce12cdec
Lakshman <lakshman.avula@outlook.com>
2020-11-09 07:44:09 +0530

    Added even or odd logic

--- src/com/careerit/cj/day5/EvenOrOdd.java
+++ src/com/careerit/cj/day5/EvenOrOdd.java
@@ -7,5 +7,13 @@ public class EvenOrOdd {
           public static void main(String... args){
             
         	  		Scanner sc = new Scanner(System.in);
+        	  		System.out.println("Enter the num:");
+        	  		int num = sc.nextInt();
+        	  		if(num % 2 == 0) {
+        	  			System.out.println("Even");
+        	  		}else {
+        	  			System.out.println("Odd");
+        	  		}
+        	  		sc.close();
           }
 }
