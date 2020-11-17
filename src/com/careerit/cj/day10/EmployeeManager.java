f38297d4a914b062c6248153227dcd724aa02c1c
Lakshman <lakshman.avula@outlook.com>
2020-11-17 08:38:04 +0530

    Added object oriented programming.. examples

---
+++ src/com/careerit/cj/day10/EmployeeManager.java
@@ -0,0 +1,23 @@
+package com.careerit.cj.day10;
+
+public class EmployeeManager {
+
+	public static void main(String[] args) {
+
+		Employee emp1 = new Employee(1001, "Krish", 50000);
+		Employee emp2 = new Employee(1002, "Manoj", 98000);
+     	Employee emp3 = new Employee(1003, "Balu", 200000);
+    	
+     	
+     	Employee emp4 = new Employee(1004, "Lakshman", 100000);
+
+		Employee emp5 = new Employee();
+		emp5.setValues(1005, "Charan", 250000);
+		emp5.setValues(1005, "Charan", 250000);
+		emp5.setValues(1005, "Charan", 250000);
+		
+		
+		
+
+	}
+}
